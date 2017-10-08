d73c7e8e91bf13fee0e904c4439b917e164079e5
Hendrik Tillman <henktillman@gmail.com>
2017-10-07 18:43:47 -0700

    Alarm creation input

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableHighlight } from 'react-native';
+import AlarmCreator from './AlarmCreator'
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      creatingAlarm: true,
+    }
+  }
+
+  createNewAlarm = () => {
+    this.setState({creatingAlarm: true})
+  }
+
+  handleCreate = state => {
+    this.setState({creatingAlarm: false})
+    this.createAndDisplay(state)
+  }
+
+  createAndDisplay = state => {
+
+  }
+
   render() {
+    if (this.state.creatingAlarm) {
+      return <AlarmCreator handleCreate={this.handleCreate} />
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TouchableHighlight
+          style={styles.submit}
+          onPress={this.createNewAlarm}
+          underlayColor='#fff'>
+            <Text style={styles.submitText}>Create a new alarm:</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -16,6 +46,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 50,
+    borderRadius: 3,
+  },
+  submit: {
+    width: 200,
+    marginRight:40,
+    marginLeft:40,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#68a0cf',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#d3d3d3'
+  },
+  submitText:{
+    color:'#fff',
+    textAlign:'center',
   },
 });
